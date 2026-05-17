6ee36f691e5b992814d464f10a8a562dcb11d2e3
Deyyan Guven <nurdey88@gmail.com>
2026-05-17 15:57:22 +0300

    start-formation-is-done

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -56,147 +56,141 @@ func card_locate():
 	var dealt_card : int = 0 #card location line. First card goes to first column etc.
 	while dealt_card < 28:
 		var selected_card = cardsarray.pick_random()
-		var card3 = cardsarray.pick_random()
-		var card5 = cardsarray.pick_random()
-		var card6 = cardsarray.pick_random()
-		var card8 = cardsarray.pick_random()
-		var card9 = cardsarray.pick_random()
-		var card10 = cardsarray.pick_random()
-		var card12 = cardsarray.pick_random()
-		var card13 = cardsarray.pick_random()
-		var card14 = cardsarray.pick_random()
-		var card15 = cardsarray.pick_random()
-		var card17 = cardsarray.pick_random()
-		var card18 = cardsarray.pick_random()
-		var card19 = cardsarray.pick_random()
-		var card20 = cardsarray.pick_random()
-		var card21 = cardsarray.pick_random()
-		var card23 = cardsarray.pick_random()
-		var card24 = cardsarray.pick_random()
-		var card25 = cardsarray.pick_random()
-		var card26 = cardsarray.pick_random()
-		var card27 = cardsarray.pick_random()
-		var card28 = cardsarray.pick_random()
-		add_child(selected_card)
+		selected_card.z_index = 1
+		cardsarray.erase(selected_card)
 		if dealt_card == 0: #column1
 			selected_card.global_position = Vector2(92, 49)
 			dealt_card += 1
-			cardsarray.erase(selected_card)
 			print("yeh")
 		elif dealt_card == 1: #column2
 			selected_card.global_position = Vector2(152, 49)
 			dealt_card += 1
-			cardsarray.erase(selected_card)
+			var card3 = cardsarray.pick_random()
+			cardsarray.erase(card3)
+			card3.z_index = 2
 			print(card3.name)
 			card3.global_position = Vector2(152, 64)
 			print(card3.global_position)
-			selected_card.add_child(card3)
 			print(card3.get_parent().name)
 			dealt_card += 1
-			cardsarray.erase(card3)
 			print("yeh")
 		elif dealt_card == 3: #column3
 			selected_card.global_position = Vector2(212, 49)
 			dealt_card += 1
-			cardsarray.erase(selected_card)
-			card5.global_position = Vector2(212, 64)
-			selected_card.add_child(card5)
-			dealt_card += 1
+			var card5 = cardsarray.pick_random()
 			cardsarray.erase(card5)
-			card6.global_position = Vector2(212, 79)
-			card5.add_child(card6)
-			dealt_card += 1
+			card5.z_index = 2
+			var card6 = cardsarray.pick_random()
 			cardsarray.erase(card6)
+			card6.z_index = 3
+			card5.global_position = Vector2(212, 64)
+			dealt_card += 1
+			card6.global_position = Vector2(212, 79)
+			dealt_card += 1
 			print("yeh")
 		elif dealt_card == 6: #column4
 			selected_card.global_position = Vector2(272, 49)
 			dealt_card += 1
-			cardsarray.erase(selected_card)
-			card8.global_position = Vector2(272, 64)
-			selected_card.add_child(card8)
-			dealt_card += 1
+			var card8 = cardsarray.pick_random()
 			cardsarray.erase(card8)
-			card9.global_position = Vector2(272, 79)
-			card8.add_child(card9)
-			dealt_card += 1
+			card8.z_index = 2
+			var card9 = cardsarray.pick_random()
 			cardsarray.erase(card9)
-			card10.global_position = Vector2(272, 94)
-			card9.add_child(card10)
-			dealt_card += 1
+			card9.z_index = 3
+			var card10 = cardsarray.pick_random()
 			cardsarray.erase(card10)
+			card10.z_index = 4
+			card8.global_position = Vector2(272, 64)
+			dealt_card += 1
+			card9.global_position = Vector2(272, 79)
+			dealt_card += 1
+			card10.global_position = Vector2(272, 94)
+			dealt_card += 1
 			print("yeh")
 		elif dealt_card == 10: #column5
 			selected_card.global_position = Vector2(332, 49)
 			dealt_card += 1
-			cardsarray.erase(selected_card)
-			card12.global_position = Vector2(332, 64)
-			selected_card.add_child(card12)
-			dealt_card += 1
+			var card12 = cardsarray.pick_random()
 			cardsarray.erase(card12)
-			card13.global_position = Vector2(332, 79)
-			card12.add_child(card13)
-			dealt_card += 1
+			card12.z_index = 2
+			var card13 = cardsarray.pick_random()
 			cardsarray.erase(card13)
-			card14.global_position = Vector2(332, 94)
-			card13.add_child(card14)
-			dealt_card += 1
+			card13.z_index = 3
+			var card14 = cardsarray.pick_random()
 			cardsarray.erase(card14)
-			card15.global_position = Vector2(332, 109)
-			card14.add_child(card15)
-			dealt_card += 1
+			card14.z_index = 4
+			var card15 = cardsarray.pick_random()
 			cardsarray.erase(card15)
+			card15.z_index = 5
+			card12.global_position = Vector2(332, 64)
+			dealt_card += 1
+			card13.global_position = Vector2(332, 79)
+			dealt_card += 1
+			card14.global_position = Vector2(332, 94)
+			dealt_card += 1
+			card15.global_position = Vector2(332, 109)
+			dealt_card += 1
 			print("yeh")
 		elif dealt_card == 15: #column6
 			selected_card.global_position = Vector2(392, 49)
 			dealt_card += 1
-			cardsarray.erase(selected_card)
-			card17.global_position = Vector2(392, 64)
-			selected_card.add_child(card17)
-			dealt_card += 1
+			var card17 = cardsarray.pick_random()
 			cardsarray.erase(card17)
-			card18.global_position = Vector2(392, 79)
-			card17.add_child(card18)
-			dealt_card += 1
+			card17.z_index = 2
+			var card18 = cardsarray.pick_random()
 			cardsarray.erase(card18)
-			card19.global_position = Vector2(392, 94)
-			card18.add_child(card19)
-			dealt_card += 1
+			card18.z_index = 3
+			var card19 = cardsarray.pick_random()
 			cardsarray.erase(card19)
-			card20.global_position = Vector2(392, 109)
-			card19.add_child(card20)
-			dealt_card += 1
+			card19.z_index = 4
+			var card20 = cardsarray.pick_random()
 			cardsarray.erase(card20)
-			card21.global_position = Vector2(392, 124)
-			card20.add_child(card21)
-			dealt_card += 1
+			card20.z_index = 5
+			var card21 = cardsarray.pick_random()
 			cardsarray.erase(card21)
+			card21.z_index = 6
+			card17.global_position = Vector2(392, 64)
+			dealt_card += 1
+			card18.global_position = Vector2(392, 79)
+			dealt_card += 1
+			card19.global_position = Vector2(392, 94)
+			dealt_card += 1
+			card20.global_position = Vector2(392, 109)
+			dealt_card += 1
+			card21.global_position = Vector2(392, 124)
+			dealt_card += 1
 			print("yeh")
 		elif dealt_card == 21: #column7
 			selected_card.global_position = Vector2(452, 49)
 			dealt_card += 1
-			cardsarray.erase(selected_card)
-			card23.global_position = Vector2(452, 64)
-			selected_card.add_child(card23)
-			dealt_card += 1
+			var card23 = cardsarray.pick_random()
 			cardsarray.erase(card23)
-			card24.global_position = Vector2(452, 79)
-			card23.add_child(card24)
-			dealt_card += 1
+			card23.z_index = 2
+			var card24 = cardsarray.pick_random()
 			cardsarray.erase(card24)
-			card25.global_position = Vector2(452, 94)
-			card24.add_child(card25)
-			dealt_card += 1
+			card24.z_index = 3
+			var card25 = cardsarray.pick_random()
 			cardsarray.erase(card25)
-			card26.global_position = Vector2(452, 109)
-			card25.add_child(card26)
-			dealt_card += 1
+			card25.z_index = 4
+			var card26 = cardsarray.pick_random()
 			cardsarray.erase(card26)
-			card27.global_position = Vector2(452, 124)
-			card26.add_child(card27)
-			dealt_card += 1
+			card26.z_index = 5
+			var card27 = cardsarray.pick_random()
 			cardsarray.erase(card27)
-			card28.global_position = Vector2(452, 139)
-			card27.add_child(card28)
-			dealt_card += 1
+			card27.z_index = 6
+			var card28 = cardsarray.pick_random()
 			cardsarray.erase(card28)
+			card28.z_index = 7
+			card23.global_position = Vector2(452, 64)
+			dealt_card += 1
+			card24.global_position = Vector2(452, 79)
+			dealt_card += 1
+			card25.global_position = Vector2(452, 94)
+			dealt_card += 1
+			card26.global_position = Vector2(452, 109)
+			dealt_card += 1
+			card27.global_position = Vector2(452, 124)
+			dealt_card += 1
+			card28.global_position = Vector2(452, 139)
+			dealt_card += 1
 			print("yeh")
